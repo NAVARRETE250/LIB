6f1221a00217643815e146e1ff3f58aec813dd00
Pau <73888436+PauRuiz19@users.noreply.github.com>
2021-11-05 17:02:33 +0100

    Add files via upload

---
+++ Hib_Kahoot/src/main/java/hibernate/model/Concurso.java
@@ -0,0 +1,107 @@
+package hibernate.model;
+
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "concurso")
+public class Concurso {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id_concurso")
+	private int id_concurso;
+	
+	@ManyToOne()
+	@JoinColumn(name = "id_kahoot")
+	private Kahoot id_kahoot;
+	
+	@ManyToMany(mappedBy = "concurso")
+    private Set<Concursante> concursantes;
+	
+	@Column(name = "ganador")
+	private int ganador;
+	
+	@Column(name = "segundo")
+	private int segundo;
+	
+	@Column(name = "tercero")
+	private int tercero;
+
+	public Concurso() {
+		super();
+	}
+
+	public Concurso(int id_concurso, Kahoot id_kahoot, Set<Concursante> concursantes, int ganador, int segundo,
+			int tercero) {
+		super();
+		this.id_concurso = id_concurso;
+		this.id_kahoot = id_kahoot;
+		this.concursantes = concursantes;
+		this.ganador = ganador;
+		this.segundo = segundo;
+		this.tercero = tercero;
+	}
+
+	public int getId_concurso() {
+		return id_concurso;
+	}
+
+	public void setId_concurso(int id_concurso) {
+		this.id_concurso = id_concurso;
+	}
+
+	public Kahoot getId_kahoot() {
+		return id_kahoot;
+	}
+
+	public void setId_kahoot(Kahoot id_kahoot) {
+		this.id_kahoot = id_kahoot;
+	}
+
+	public Set<Concursante> getConcursantes() {
+		return concursantes;
+	}
+
+	public void setConcursantes(Set<Concursante> concursantes) {
+		this.concursantes = concursantes;
+	}
+
+	public int getGanador() {
+		return ganador;
+	}
+
+	public void setGanador(int ganador) {
+		this.ganador = ganador;
+	}
+
+	public int getSegundo() {
+		return segundo;
+	}
+
+	public void setSegundo(int segundo) {
+		this.segundo = segundo;
+	}
+
+	public int getTercero() {
+		return tercero;
+	}
+
+	public void setTercero(int tercero) {
+		this.tercero = tercero;
+	}
+	
+	
+
+}
---
+++ Hib_Kahoot/src/main/java/hibernate/model/Usuario.java
@@ -0,0 +1,69 @@
+package hibernate.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id_usuario")
+	private int id_usuario;
+	
+	@Column(name = "nombre")
+	private String nombre;
+	
+	@Column(name = "contraseña")
+	private String contraseña;
+	
+	@OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Kahoot> kahoots;
+
+	public Usuario() {
+		super();
+	}
+
+	public Usuario(int id_usuario, String nombre, String contraseña) {
+		super();
+		this.id_usuario = id_usuario;
+		this.nombre = nombre;
+		this.contraseña = contraseña;
+	}
+
+	public int getId_usuario() {
+		return id_usuario;
+	}
+
+	public void setId_usuario(int id_usuario) {
+		this.id_usuario = id_usuario;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getContraseña() {
+		return contraseña;
+	}
+
+	public void setContraseña(String contraseña) {
+		this.contraseña = contraseña;
+	}
+	
+	
+
+}
